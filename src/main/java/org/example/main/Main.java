bd3886687833828049cd29f86db7d7e0d3ce1c4a
cutiepepe29266 <cutiepepe2926@gmail.com>
2025-12-25 21:34:25 +0900

    12-25 add Bean by using Programming Method

--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -1,13 +1,20 @@
 package org.example.main;
 
+import java.util.function.Supplier;
 import org.example.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Parrot p = context.getBean(Parrot.class);
-        System.out.println(p);
-        System.out.println(p.getName());
+        Parrot p = new Parrot();
+        p.setName("Kiki");
+
+        Supplier<Parrot> parrotSupplier = () -> p;
+
+        context.registerBean("parrot1", Parrot.class, parrotSupplier);
+
+        Parrot x = context.getBean(Parrot.class);
+        System.out.println(x.getName());
     }
 }
--- src/main/java/org/example/main/Parrot.java
+++ src/main/java/org/example/main/Parrot.java
@@ -1,16 +1,7 @@
 package org.example.main;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.stereotype.Component;
-
-@Component
 public class Parrot {
     private String name;
-
-    @PostConstruct
-    public void init() {
-        this.name = "Parrot";
-    }
 
     public String getName() {
         return name;
